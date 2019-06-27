e0a29580ebf075a818221334de15e535f87d21a5
hustlefilled <52221920+hustlefilled@users.noreply.github.com>
2019-06-27 16:18:19 -0400

    Update bypasser.py

--- instabot.py-master/instabot_py/bypasser.py
+++ instabot.py-master/instabot_py/bypasser.py
@@ -63,7 +63,7 @@ def _reset_challenge(_bot):
 def _solve_checkpoint_challenge(_bot):
     # --- Start challenge
     time.sleep(3)
-    challenge_url = ['challenge']['api_path'][1:]
+    challenge_url = https://instagram.com/challenge/1121798310/0mY6aoNAts/
     try:
         _bot.send_request(
             challenge_url, None, login=True, with_signature=False)
